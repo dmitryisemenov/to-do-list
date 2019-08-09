f9ad9ef7035a49b2a6ab6cb6788a43ec0d963de8
dmitryisemenov <dmitriysemenov1994th@gmail.com>
2019-08-09 11:51:33 +0300

    done

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import deleteButton from './icons/deleteButton.png'
 
-const TodoList = ({todos, deleteTodo}) => (
-    <div>
-        {todos.map((todo, index) => (
-            <div style={{display: 'flex', width: '200px', marginTop: '20px'}}>
-                <input type='checkbox' onChange={() => {console.log(todo)}} />
-                <div style={{marginLeft: '30px', textDecoration: 'line-through'}}>{ todo }</div>
-                <img style={{marginLeft: '30px', width: '20px', height: '20px'}} src={deleteButton} alt='deleteButton' onClick={() => {
-                    deleteTodo(index);
-                }} />
-            </div>
-        ))}
-    </div>
-)
+const TodoList = ({todos, deleteTodo}) => {
+    const [isCompleted, toggleCheck] = React.useState(false)
+    return (
+        <div>
+            {todos.map((todo, index) => (
+                <div style={{display: 'flex', width: '300px', marginTop: '20px'}}>
+                    <input type='checkbox' onChange={() => toggleCheck(!isCompleted)} />
+                    <div style={{marginLeft: '30px', textDecoration: isCompleted ? 'line-through' : ''}}>{ todo }</div>
+                    <img style={{marginLeft: '30px', width: '20px', height: '20px'}} src={deleteButton} alt='deleteButton' onClick={() => {
+                        deleteTodo(index);
+                    }} />
+                </div>
+            ))}
+        </div>
+    )
+    
+}
 
 export default TodoList;
