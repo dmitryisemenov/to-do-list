cd197c4295757e6a3568e224981f90525e2cd90f
dmitryisemenov <dmitriysemenov1994th@gmail.com>
2019-08-09 09:34:38 +0300

    second

---
+++ src/TodoForm.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+
+const TodoForm = ({ saveTodo }) => {
+    const [value, setValue] = useState('');
+
+    return (
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            saveTodo(value);
+        }}>
+            <input variant='outlined' placeholder='Add Todo' margin='normal'
+            onChange={(event) => {
+                setValue(event.target.value);
+            }}
+            value={value}></input>
+        </form>
+    )
+}
+
+export default TodoForm
